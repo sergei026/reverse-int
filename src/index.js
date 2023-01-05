7bb7631337d19a0c47718be95d41c12a663a0839
sergei026 <sergeyburukhin@ya.ru>
2023-01-05 10:15:31 +0400

    feat: submit task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let str = String(n);
+  let result = '';
+  let i = str.length - 1;
+  while (i >=  0) {
+    result = `${result}${str[i]}`;
+    i = i - 1;
+  }
+  return result * 1;
 }
